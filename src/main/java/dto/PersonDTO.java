b94af053fe2158c1c68e338129dd76060503f45b
Sinan <sinanjasar@live.dk>
2019-10-09 19:27:11 +0200

    moved DTOmapper to PersonDTO

--- src/main/java/dto/PersonDTO.java
+++ src/main/java/dto/PersonDTO.java
@@ -1,5 +1,7 @@
 package dto;
 
+import entities.Address;
+import entities.CityInfo;
 import entities.Hobby;
 import entities.Person;
 import entities.Phone;
@@ -131,5 +133,29 @@ public class PersonDTO {
         this.city = city;
     }
 
+    public Person DTOMapper(PersonDTO personDTO) {
+        
+        CityInfo cityInfo = new CityInfo(personDTO.getZip(), personDTO.getCity());
+        Address address = new Address(cityInfo, personDTO.getStreet(), personDTO.getStreetInfo());
+        List<Hobby> hobbies = new ArrayList();
+        List<Phone> phones = new ArrayList();
+        //syntax for hobbies is: "name:value,description:value"
+        for (String hobby : personDTO.getHobbies()) {
+            hobbies.add(new Hobby(hobby.split(",")[0].split(":")[1],
+                    hobby.split(",")[1].split(":")[1]));
+        }
+        //syntax for phones is: "number:value,description:value"
+        for (String phone : personDTO.getPhones()) {
+            phones.add(new Phone(phone.split(",")[0].split(":")[1],
+                    phone.split(",")[1].split(":")[1]));
+        }
+        Person p = new Person(personDTO.getEmail(), personDTO.getFirstName(),
+                personDTO.getLastName(), phones, address, hobbies);
+        
+        for(Phone phone : phones) {
+            phone.setPerson(p);
+        }
+        return p;
+    }
 
 }
